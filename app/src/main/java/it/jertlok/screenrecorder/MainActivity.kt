2cad5528e51a252596d3a21a689497e4178c3d7e
Giuseppe Maggio <jertlok@protonmail.com>
2019-04-26 13:06:02 +0200

    Slightly improve user experience
    The whole UI still sucks, but it's definitely better
    than it was before.
    Also improve some interactions.

--- app/src/main/java/it/jertlok/screenrecorder/MainActivity.kt
+++ app/src/main/java/it/jertlok/screenrecorder/MainActivity.kt
@@ -1,19 +1,26 @@
 package it.jertlok.screenrecorder
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
 import android.media.projection.MediaProjectionManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        val TAG = "MainActivity"
+        // val TAG = "MainActivity"
+        // Permission request code
+        private const val PERMISSION_REQUEST_WRITE_EXTERNAL = 0
+        // Request code for starting a screen record
         private const val REQUEST_CODE_SCREEN_RECORD = 1
     }
 
@@ -21,12 +28,33 @@ class MainActivity : AppCompatActivity() {
     // MediaProjection API
     private lateinit var mMediaProjectionManager: MediaProjectionManager
     // User interface
-    private lateinit var startButton: Button
-    private lateinit var stopButton: Button
+    private lateinit var bottomBar: BottomAppBar
+    private lateinit var fabButton: FloatingActionButton
+    // Drawables
+    private var fabStartDrawable: Drawable? = null
+    private var fabStopDrawable: Drawable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // User interface
+        bottomBar = findViewById(R.id.bar)
+        fabButton = findViewById(R.id.fab)
+        // Drawables
+        fabStartDrawable = getDrawable(R.drawable.ic_outline_record)
+        fabStopDrawable = getDrawable(R.drawable.ic_outline_stop)
+
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // Show the explanation
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        PERMISSION_REQUEST_WRITE_EXTERNAL)
+            }
+        }
 
         // Instantiate Screen Recorder class
         mScreenRecorder = ScreenRecorder(applicationContext)
@@ -35,25 +63,20 @@ class MainActivity : AppCompatActivity() {
         mMediaProjectionManager = applicationContext.getSystemService(
                 Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
 
-        // User interface
-        startButton = findViewById(R.id.start_button)
-        stopButton = findViewById(R.id.stop_button)
-        // Set on click listener for starting the recording
-        startButton.setOnClickListener {
-            // Request to user the screen cast permission
-            val recorderIntent = mMediaProjectionManager.createScreenCaptureIntent()
-            startActivityForResult(recorderIntent, REQUEST_CODE_SCREEN_RECORD)
-        }
-        // Set on click listener for stopping the recording
-        stopButton.setOnClickListener {
-            // Stop recording
-            mScreenRecorder.stopRecording()
-            // TODO: Make this check a little bit more reliable
-            // If the file got created
-            if (mScreenRecorder.mOutputFile != null) {
-                val contentUri = Uri.fromFile(mScreenRecorder.mOutputFile)
-                val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, contentUri)
-                sendBroadcast(mediaScanIntent)
+        // Set actions for the FAB
+        fabButton.setOnClickListener {
+            // Here we need to understand whether we are recording or not.
+            // If we are not recording we can send the intent for recording
+            // otherwise we will try to stop the recording.
+            if (!mScreenRecorder.isRecording()) {
+                val recorderIntent = mMediaProjectionManager.createScreenCaptureIntent()
+                startActivityForResult(recorderIntent, REQUEST_CODE_SCREEN_RECORD)
+            } else {
+                mScreenRecorder.stopRecording()
+                // Let's reset the FAB icon to start
+                fabButton.setImageDrawable(fabStartDrawable)
+                // Try to notify that we have created a new file
+                notifyNewMedia(mScreenRecorder.getOutputFileName())
             }
         }
     }
@@ -61,14 +84,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        // TODO: Create some toasts?
         if (requestCode == REQUEST_CODE_SCREEN_RECORD) {
             if (resultCode != Activity.RESULT_OK) {
                 // The user did not grant the permission
+                Toast.makeText(this, getString(R.string.permission_cast_denied),
+                        Toast.LENGTH_SHORT).show()
                 return
             }
             // Otherwise we can start the screen record
             mScreenRecorder.startRecording(resultCode, data)
+            // We need to toggle the fab button
+            fabButton.setImageDrawable(fabStopDrawable)
+        }
+    }
+
+    private fun notifyNewMedia(filePath: String?) {
+        // TODO: make a better check
+        if (filePath != null) {
+            val contentUri = Uri.fromFile(mScreenRecorder.mOutputFile)
+            val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, contentUri)
+            sendBroadcast(mediaScanIntent)
         }
     }
 }
