eb3fe3e59fe9b7ce71a10177df738a5314e70dad
Giuseppe Maggio <jertlok@protonmail.com>
2019-05-02 11:22:10 +0200

    Remove video resolution preference
    In my humble opinion it doesn't make any sense to mess with
    the native screen resolution. If the phone can't record its
    surface, then the user can change the framerate and bitrate.

--- app/src/main/java/it/jertlok/screenrecorder/fragments/SettingsFragment.kt
+++ app/src/main/java/it/jertlok/screenrecorder/fragments/SettingsFragment.kt
@@ -23,7 +23,6 @@ class SettingsFragment: PreferenceFragmentCompat() {
     private lateinit var mSharedPreferences: SharedPreferences
 
     // User interface
-    private lateinit var videoListPref: ListPreference
     private lateinit var frameRateListPref: ListPreference
     private lateinit var audioRecordingPref: SwitchPreference
 
@@ -44,41 +43,9 @@ class SettingsFragment: PreferenceFragmentCompat() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // TODO: we also need to set the descriptions.
-        videoListPref = findPreference("video_resolution_pref") as ListPreference
         frameRateListPref = findPreference("frame_rate_pref") as ListPreference
         audioRecordingPref = findPreference("audio_recording_pref") as SwitchPreference
 
-        // TODO: Let's remove the XML arrays and put programmatically arrays
-        // TODO: down here or some sort of Settings controller.
-        // TODO: as it doesn't make any sense to play this lil' game.
-        // We need to inject to the videoListPreference the mDisplayRes
-        if (!videoListPref.entryValues.contains(mDisplayRes)) {
-            videoListPref.entryValues[0] = mDisplayRes
-        } else {
-            // Exclude "Device screen resolution" as it's a supported one, which means
-            // that it's inside our list.
-            val supportedVideoEntries = Arrays.copyOfRange(videoListPref.entries, 1,
-                    videoListPref.entries.size)
-            val supportedVideoEntryValues = Arrays.copyOfRange(videoListPref.entryValues, 1,
-                    videoListPref.entryValues.size)
-            // Let's set the values
-            videoListPref.entries = supportedVideoEntries
-            videoListPref.entryValues = supportedVideoEntryValues
-        }
-
-        // We need to get the shared preferences and change the elements accordingly
-        videoListPref.setValueIndex(videoListPref.findIndexOfValue(
-                mSharedPreferences.getString("video_resolution", mDisplayRes)))
-        frameRateListPref.setValueIndex(frameRateListPref.findIndexOfValue(
-                mSharedPreferences.getString("frame_rate", "30")))
-        audioRecordingPref.isChecked =
-                mSharedPreferences.getBoolean("audio_recording", false)
-
-        // Set on list preference change listeners, that will write into the SharedPreferences
-        videoListPref.setOnPreferenceChangeListener { _, newValue ->
-            mSharedPreferences.edit().putString("video_resolution", newValue as String).apply()
-            true
-        }
         frameRateListPref.setOnPreferenceChangeListener { _, newValue ->
             mSharedPreferences.edit().putString("frame_rate", newValue as String).apply()
             true
