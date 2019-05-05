bc843ec6e866ac648df58d0e8d7de87557a2fc85
Giuseppe Maggio <jertlok@protonmail.com>
2019-05-05 12:58:45 +0200

    UI improvements
    Try to follow the Google guidelines, remove hardcoded values
    fix the RecordinActivity StatusBar / Navigation bar color
    especially for Android O and superiror and try to make the various
    layouts better (there were some redundancy - I suck with layouts).

--- app/src/main/java/it/jertlok/screenrecorder/activities/RecordingActivity.kt
+++ app/src/main/java/it/jertlok/screenrecorder/activities/RecordingActivity.kt
@@ -3,9 +3,13 @@ package it.jertlok.screenrecorder.activities
 import android.app.*
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.media.projection.MediaProjectionManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import it.jertlok.screenrecorder.R
@@ -17,6 +21,23 @@ open class RecordingActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // TODO: move to when or something easier to read
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val baseFlags = WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            // Marshmallow conditions
+            window.decorView.systemUiVisibility = baseFlags
+            // If it's higher than O we need to add something else
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                window.decorView.systemUiVisibility = baseFlags or
+                        View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+
+            }
+            // Set status bar color
+            window.statusBarColor = Color.TRANSPARENT
+        }
+
         val mediaProjectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE)
                 as MediaProjectionManager
         mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
